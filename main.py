e4dd6f9281bcc13b5e26869a1509d87eed16937c
Matthew Gunton <mgunton7@gmail.com>
2024-07-20 10:01:31 -0500

    output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import PyPDF2
+import re
 
 def read_pdf(file_path):
     with open(file_path, 'rb') as file:
@@ -7,11 +8,12 @@ def read_pdf(file_path):
         text = ""
         for page in reader.pages:
             text += page.extract_text() + "\n"
+    output_file = "out.txt"
+    with open(output_file, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(text)
     return text
 
 source_file = "./2023-Apartment-Lease-Contract-12-23-SAMPLE.pdf"
+map_section_to_text = {}
 data = read_pdf(source_file)
 
-
-
-print(data)
